7866b6c142cf539a3b995a59c44b5f96720e431e
Vivek9988 <vivek19102004@gmail.com>
2025-05-28 10:46:27 +0530

    Context Api

---
+++ Frontend/src/Component/Context.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useContext } from 'react';
+
+const FilterContext = createContext();
+
+export const FilterProvider = ({ children }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedRating, setSelectedRating] = useState('');
+    const [selectedStyles, setSelectedStyles] = useState([]);
+    const [selectedCity, setSelectedCity] = useState('');
+    const [sortOption, setSortOption] = useState('');
+
+    return (
+        <FilterContext.Provider
+            value={{
+                searchTerm, setSearchTerm,
+                selectedRating, setSelectedRating,
+                selectedStyles, setSelectedStyles,
+                selectedCity, setSelectedCity,
+                sortOption, setSortOption,
+            }}
+        >
+            {children}
+        </FilterContext.Provider>
+    );
+};
+
+export const useFilters = () => useContext(FilterContext);
